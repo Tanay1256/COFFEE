cf9ea6c543bcf087d4e4572685067fb923ad59b2
Tanay1256 <130854472+Tanay1256@users.noreply.github.com>
2023-04-15 21:54:08 +0530

    Add files via upload

---
+++ html_css/index.js
@@ -0,0 +1,13 @@
+function trueOrFalse(isItTrue)
+{
+    if(isItTrue)
+    {
+    return "Yes its true";    
+    }
+    return "No its not true";
+}
+console.log(trueOrFalse(true));
+
+var prop={"name":"tanay","age":19,"year":2023};
+var data= prop["name"];
+console.log(data);
